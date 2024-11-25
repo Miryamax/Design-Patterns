5dc75e13f31100f7c802a5c9a92b20ddf1f746b0
Miryam Maximov <miryamma@etoro.com>
2024-11-25 13:58:38 +0200

    add strategy implementation

--- Strategy/Implementation.cs
+++ Strategy/Implementation.cs
@@ -1,3 +1,48 @@
-﻿namespace Strategy
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Strategy
 {
+    // the strategy
+    public interface IExportService
+    {
+        void Export(Order order);
+    }
+
+    public class CSVExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine("export to CSV file");
+        }
+    }
+    public class XMLExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine("export to XML file");
+        }
+    }
+
+    public class JsonExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine("export to Json file");
+        }
+    }
+
+    public class Order
+    {
+        private IExportService _exportService;
+
+        public Order(IExportService exportService)
+        {
+            _exportService = exportService;
+        }
+
+        public void Export()
+        {
+            _exportService.Export(this);
+        }
+    }
 }
