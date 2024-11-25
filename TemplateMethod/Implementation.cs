f8c43ee92c33a99964e287fbdc9e4f635c50b9f2
Miryam Maximov <miryamma@etoro.com>
2024-11-25 22:34:05 +0200

    implement the memento pattern

--- TemplateMethod/Implementation.cs
+++ TemplateMethod/Implementation.cs
@@ -1,3 +1,51 @@
-﻿namespace TemplateMethod
-{ 
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TemplateMethod
+{
+    public abstract class MailParser
+    {
+        public virtual string ParseMailBody(int identifier)
+        {
+            return "Parse mail body";
+        }
+
+        public virtual void FindServer()
+        {
+            Console.WriteLine("find server...");
+        }
+
+        public virtual void AuthenticateToServer()
+        {
+            Console.WriteLine("Authenticate to Server...");
+        }
+
+        public string ParseHTMLBody(int identifier)
+        {
+            return "Parse HTML body";
+        }
+    }
+
+    public class ExchangeMailParser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            base.AuthenticateToServer();
+            Console.WriteLine("from exchange mail parser");
+        }
+    }
+
+    public class EudoraMailParser : MailParser
+    {
+        public override void FindServer()
+        {
+            Console.WriteLine("find server...");
+        }
+
+        public override void AuthenticateToServer()
+        {
+            base.AuthenticateToServer();
+            Console.WriteLine("from eudora mail parser");
+        }
+
+    }
 }
