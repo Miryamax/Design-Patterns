b6dce412cd53597447836b4b440a9efe3244f064
Miryam Maximov <miryamma@etoro.com>
2024-11-25 22:23:51 +0200

    implement the command pattern

---
+++ Command/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class Employee
+    {
+        private static int counter = 0;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Employee(string name)
+        {
+            Id = ++counter;
+            Name = name;
+        }
+    }
+}
