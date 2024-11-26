2b7b380c3412b391bcb1e0661c51776b568f466b
Miryam Maximov <miryamma@etoro.com>
2024-11-26 15:57:30 +0200

    implement iterator pattern

---
+++ Iterator/Interfaces/IPeopleIterator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator.Interfaces
+{
+    public interface IPeopleIterator
+    {
+        Person First();
+        Person Next();
+        bool IsDone();
+        Person CurrentItem { get; }
+    }
+}
