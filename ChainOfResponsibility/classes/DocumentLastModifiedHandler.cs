793b950487898477762bc09d9488a0ee49c04dcc
Miryam Maximov <miryamma@etoro.com>
2024-11-26 12:31:18 +0200

    implement the chainOfResponsibilty pattern

---
+++ ChainOfResponsibility/classes/DocumentLastModifiedHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChainOfResponsibility.Interfaces;
+
+namespace ChainOfResponsibility.classes
+{
+    public class DocumentLastModifiedHandler : IHandler<Document>
+    {
+        private IHandler<Document>? _successor;
+        public void Handle(Document request)
+        {
+            // here some logic - validations
+            // and go to the next handle 
+            _successor?.Handle(request);
+        }
+
+        public IHandler<Document> SetSuccessor(IHandler<Document> successor)
+        {
+            _successor = successor;
+            return _successor;
+        }
+    }
+}
