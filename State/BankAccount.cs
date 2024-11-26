09c82ae364f611e5b6b20171754214dec7f247ff
Miryam Maximov <miryamma@etoro.com>
2024-11-26 14:30:58 +0200

    implement state pattern

---
+++ State/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public abstract class BankAccount
+    {
+        public BankAccountState BankAccountState { get; set;  }
+
+        public BankAccount(BankAccountState bankAccountState)
+        {
+            BankAccountState = bankAccountState;
+        }
+        public void Deposit(decimal amount)
+        {
+            BankAccountState.Deposit(amount);
+        }
+
+        public void WithDraw(decimal amount)
+        {
+            BankAccountState.WithDraw(amount);
+        }
+    }
+}
