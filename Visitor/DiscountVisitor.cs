8f6197c641621030dc88bdf217252ec8c3c5ac4e
Miryam Maximov <miryamma@etoro.com>
2024-11-26 16:21:18 +0200

    implement visitor pattern

---
+++ Visitor/DiscountVisitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Visitor.Interfaces;
+
+namespace Visitor
+{
+    public class DiscountVisitor : IVisitor
+    {
+        public void VisitCustomer(Customer customer)
+        {
+            // here some specific logic to calculate the discount for the customer
+            customer.Discount += 20;
+        }
+
+        public void VisitEmployee(Employee employee)
+        {
+            // here some specific logic to calculate the discount for the employee
+            employee.Discount += 10;
+        }
+    }
+}
---
+++ Visitor/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Visitor.Interfaces;
+
+namespace Visitor
+{
+    public class Employee : IElement
+    {
+        public decimal Discount { get; set; }
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitEmployee(this);
+        }
+    }
+}
